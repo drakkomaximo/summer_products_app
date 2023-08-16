585b41be36c4c5e9e6a042306dc5f96990fa7cd1
drakkomaximo <drakkomaximo1234@gmail.com>
2023-08-16 01:13:38 -0500

    Start new project

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 export const HomePage = () => {
   return (
     <div className="flex justify-center items-center w-full h-screen">
-      <h1 className="text-7xl capitalize font-bold">homePage</h1>
+      <h1 className="text-7xl capitalize font-bold">homePage summer app</h1>
     </div>
   )
 }
